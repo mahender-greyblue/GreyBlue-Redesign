e7beebadba05c817b65318832e8b81fdf4c4e3f7
mahender <mahender.pal@greyblue.in>
2023-11-09 19:38:40 +0530

    Responsive Started

---
+++ script/script.js
@@ -0,0 +1,10 @@
+//smooth scrolling
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+const impact_click = document.getElementById('impact-nav').onclick = function(){
+    
+}
