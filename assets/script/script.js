a0d523db5782cd8b1d062d0a98906f7df5d927e9
mahender <mahender.pal@greyblue.in>
2023-11-21 19:12:39 +0530

    live version

---
+++ assets/script/script.js
@@ -0,0 +1,25 @@
+//smooth scrolling
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+
+// Shery.textAnimate(".impact-text h2" , {
+   
+//     style: 1,
+//     y: 10,
+//     delay: 0.1,
+//     duration: 1,
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    
+//   });
+
+// Shery.mouseFollower({
+    
+//     skew: true,
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     duration: .1,
+//   });
